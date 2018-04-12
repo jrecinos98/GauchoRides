882e8f59f7ae9166dd2f2c3c87ac43a7611f447e
Jose <jrecinos@umail.ucsb.edu>
2018-04-12 15:54:10 -0700

    fixed all dependency and gradle issues

--- android/app/src/main/java/com/gauchorides/MainApplication.java
+++ android/app/src/main/java/com/gauchorides/MainApplication.java
@@ -2,7 +2,12 @@ package com.gauchorides;
 
 import android.app.Application;
 
+import com.facebook.FacebookSdk;
+import com.facebook.CallbackManager;
+import com.facebook.appevents.AppEventsLogger;
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -16,20 +21,21 @@ import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
-
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
-            new VectorIconsPackage(),
-            new VectorIconsPackage(),
+            new FBSDKPackage(mCallbackManager),
             new VectorIconsPackage()
       );
     }
