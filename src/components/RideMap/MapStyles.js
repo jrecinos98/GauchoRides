cd729d556c6611a1c4e2daec24233d33b23df538
Jose <jrecinos@umail.ucsb.edu>
2018-04-18 00:15:29 -0700

    modified android manifest

--- src/components/RideMap/MapStyles.js
+++ src/components/RideMap/MapStyles.js
@@ -2,9 +2,13 @@ import { StyleSheet } from "react-native";
 
 const styles = {
 	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center'
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
 	},
     map: {
         position: 'absolute',
