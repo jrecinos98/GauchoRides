a6a084c02eb7e8e5d190e296c11173692ad24da9
Wei Tung Chen <lleeoochen@gmail.com>
2018-04-18 12:45:12 -0700

    Updated actor classes

--- src/actors/Ride.js
+++ src/actors/Ride.js
@@ -1,10 +1,20 @@
-function Ride (id, title, description, requirement, driver, riders, time, route) {
-	this.id = id;
-	this.title = title;
-	this.description = description;
-	this.requirement = requirement; // list of tags
-	this.driver = driver; // id
-	this.riders = riders; // list of ids
-	this.time = time;
-	this.route = route;
+function Ride (id, title, description, driver, riders, time, origin, destination) {
+	this.id = id; //long
+	this.title = title; //string
+	this.description = description; //string
+	this.driver = driver; // id (long)
+	this.riders = riders; // list of ids (long)
+	this.time = time; // epoch (long)
+	this.origin = origin; // Area type
+	this.destination = destination; // Area type
+}
+
+function Area(location, radius) {
+	this.location = location; // Location type
+	this.radius = radius; // in miles (float)
+}
+
+function Location(latitude, longtitude) {
+	this.latitude = latitude; // float
+	this.longtitude = longtitude; // float
 }
--- src/actors/User.js
+++ src/actors/User.js
@@ -1,7 +1,13 @@
-function User () {
-	this.id = id;
-	this.name = name;
+function User (id, name, email, description, picture, rating, drive_rating, tags, rides, follows) {
+	this.id = id; // long
+	this.name = name; // string
+	this.email = email; // string
+	this.description = description; // string
 	this.picture = picture; // url
-	this.rating = rating;
-	this.rides = rides; // list of ids
+	this.rating = rating; // float
+	this.drive_rating = drive_rating; // float
+	this.tags = tags; // list of string (maybe make a set of enums)
+	this.rides = rides; // list of ride ids
+	this.follows = follows; // list of user ids
 }
+
