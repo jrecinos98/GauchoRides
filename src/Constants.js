198516f23afacf6708741c23031bd37785d4565a
Matt Gottlieb <Matt@Matts-Macbook-Pro.local>
2018-04-18 19:53:11 -0700

    3rd theme

--- src/Constants.js
+++ src/Constants.js
@@ -1,12 +1,18 @@
 
 // Jose
-export const COLOR_APP_BACKGROUND = '#011a42';
-export const COLOR_APP_FOCUS = '#6ec6ff';
-export const COLOR_APP_UNFOCUS = '#6ec6ff';
-export const COLOR_APP_TITLE = '#6ec6ff';
+//export const COLOR_APP_BACKGROUND = '#011a42';
+//export const COLOR_APP_FOCUS = '#6ec6ff';
+//export const COLOR_APP_UNFOCUS = '#6ec6ff';
+//export const COLOR_APP_TITLE = '#6ec6ff';
 
 // Wei 1
-// export const COLOR_APP_BACKGROUND = '#0f2236';
-// export const COLOR_APP_FOCUS = '#b3ad08';
-// export const COLOR_APP_UNFOCUS = '#c3e0c3';
-// export const COLOR_APP_TITLE = '#c3e0c3';
+//export const COLOR_APP_BACKGROUND = '#0f2236';
+//export const COLOR_APP_FOCUS = '#b3ad08';
+//export const COLOR_APP_UNFOCUS = '#c3e0c3';
+//export const COLOR_APP_TITLE = '#c3e0c3';
+
+//Matt 1
+export const COLOR_APP_BACKGROUND = '#011a42';
+export const COLOR_APP_FOCUS = '#6ec6ff';
+export const COLOR_APP_UNFOCUS = '#3498db';
+export const COLOR_APP_TITLE = '#3498db';
