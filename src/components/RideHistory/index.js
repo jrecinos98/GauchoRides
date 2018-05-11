bd625ca4346576a2a2f0d77da22a444578aa4c9a
Matt Gottlieb <Matt@Matts-Macbook-Pro.local>
2018-05-11 01:08:40 -0700

    getting rid of unnecessary code in history page

--- src/components/RideHistory/index.js
+++ src/components/RideHistory/index.js
@@ -86,27 +86,6 @@ export default class RideHistory extends Component {
 
 
     }
-  
-
-
-    makeRemoteRequest = () => {
-    const { page, seed } = this.state;
-    const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
-    this.setState({ loading: true });
-    fetch(url)
-      .then(res => res.json())
-      .then(res => {
-        this.setState({
-          data: page === 1 ? res.results : [...this.state.data, ...res.results],
-          error: res.error || null,
-          loading: false,
-          refreshing: false
-        });
-      })
-      .catch(error => {
-        this.setState({ error, loading: false });
-      });
-    };
 
     render() {
         return (
