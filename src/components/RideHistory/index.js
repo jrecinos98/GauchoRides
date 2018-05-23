28c7375a4291caa7386a60275c0152d850d5f8bd
Matt Gottlieb <Matt@169-231-129-82.wireless.ucsb.edu>
2018-05-23 12:31:22 -0700

    reloading history

--- src/components/RideHistory/index.js
+++ src/components/RideHistory/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Text, View, StyleSheet, Image, FlatList,SectionList,TouchableOpacity, ActivityIndicator} from "react-native";
+import {Text, View, StyleSheet, Image, FlatList,SectionList,TouchableOpacity, ActivityIndicator, RefreshControl} from "react-native";
 import {List, ListItem} from "react-native-elements";
 import styles from "./RideHistoryStyles.js";
 import User from "../../actors/User.js";
@@ -12,13 +12,16 @@ export default class RideHistory extends Component {
     constructor(props){
       super(props);
       this.state = {
-        loading: false,
         data: [],
         page: 1,
         seed: 1,
         error: null,
-        refreshing: false,
+        loading: false,
+        refreshing: false
+
       };
+      
+
 
       Database.getUserHistory((list) => {
          console.log(list.length);
@@ -26,11 +29,8 @@ export default class RideHistory extends Component {
       });
     }
 
-    componentDidMount(){
-      // this.makeRemoteRequest();
-    }
 
-    
+
     errData(err){
       console.log('Error!');
       console.log(err);
@@ -51,9 +51,43 @@ export default class RideHistory extends Component {
       }
     }
     */
+    componentDidMount(){
+      this.makeRemoteRequest();
+    }
 
+    makeRemoteRequest = () => {
+      const { page, seed} = this.state;
+      const url = `https://randomuser.me/api/?seed=${seed}&page=${page}&results=20`;
+      this.setState({ loading:true });
+      this.setState({loading:false, refreshing:false});
+      fetch(url)
+
+        .then(res => {
+        this.setState({
+            loading: false, 
+            refreshing: false,
+        });
+      })
+        .catch(error => {
+          this.setState({loading: false, refreshing: false,});
+
+        });
+    };
+
+
+    handleRefresh = () => {
+      this.setState({
+        page:1,
+        refreshing: true,
+        seed: this.state.seed + 1,
+      }), () => {
+        this.makeRemoteRequest();
+      }
+
+    };
 
     renderItem = ({ item }) => {
+
       console.log(item);
       return(
         <View style={{ flex: 1, flexDirection: 'row', marginBottom: 3}}>
@@ -86,21 +120,28 @@ export default class RideHistory extends Component {
 
 
     }
-  
-
-
 
     render() {
         return (
           		<View style={styles.container}>
 
           				<Text style={styles.title}>Drive History</Text>
+
+
+
                   <FlatList
                     data={this.state.data}
                     renderItem={this.renderItem}
                     keyExtractor={(item, index) => index}
                     ItemSeparatorComponent={this.renderSeparator}
+                    extraData={this.refreshing}
+                    refreshing={this.state.refreshing}
+                    onRefresh={this.handleRefresh}
+
+
                   />
+
+
 
               </View>
 
@@ -109,17 +150,5 @@ export default class RideHistory extends Component {
     }
 }
 
-/*
-                <View style={styles.rightContainer}>
-                  <TouchableOpacity style={styles.button} onPress={()=>{alert("This should go to ride page")}}>
-                    <Image source={require("../../../public/assets/plus_button.png")}
-                      style={{width: 75, height: 75}}
-                    />
 
-                  </TouchableOpacity>
-                  <Text>Create Ride</Text>
-                
-                </View>}
-
-*/
 
