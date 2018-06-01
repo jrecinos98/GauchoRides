3670f82bdeb4795755660592a3c14c7e97d66321
Matt Gottlieb <Matt@169-231-139-58.wireless.ucsb.edu>
2018-06-01 15:26:12 -0700

    messaging temporary holder

--- src/screens/MessagesScreen/index.js
+++ src/screens/MessagesScreen/index.js
@@ -1,95 +1,174 @@
 import React, { Component } from "react";
-import { StatusBar, View, Text, StyleSheet } from "react-native";
+import{ View, StyleSheet, StatusBar, Platform, Text, TouchableOpacity, ScrollView, Dimensions, Modal, AsyncStorage } from "react-native";
+import { COLOR, STRING } from '../../Constants';
+import OpacityButton from "../../components/OpacityButton";
+import CenterText from "../../components/CenterText";
 import { Ionicons } from '@expo/vector-icons';
-
-import ListView from '../../components/ListView';
-import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { COLOR, DIMENSION } from '../../Constants';
+import { MainScreenInstance } from "../Main";
 import { getTheme } from '../../Utility';
-import Database from "../../Database";
-import ListItem from "../../components/ListItem"
+import Database from '../../Database';
 
+
+//import Messenger from "../../components/Messenger";
 
 export default class MessagesScreen extends Component {
 
+    static settings_this = null;
+
     constructor(props) {
         super(props);
-        
+        settings_this = this;
+
+        settings_this.state = {
+            tabIndex: 0,
+            color_theme: COLOR.THEME_LIGHT
+        };
+
+        getTheme(function(app_theme) {
+            settings_this.setState({
+                color_theme: app_theme
+            });
+        });
+    }
+    changeAppTheme(appTheme) {
+        AsyncStorage.setItem(STRING.KEY.APP_THEME, appTheme);
+        MainScreenInstance.updateTheme();
     }
 
+    changeMapTheme(mapTheme) {
+        AsyncStorage.setItem(STRING.KEY.MAP_THEME, mapTheme);
+        MainScreenInstance.updateTheme();
+    }
 
-    static navigationOptions = {
-        tabBarIcon: ({ tintColor}) => (
-            <Ionicons name="md-book" style={{ color: tintColor, fontSize: 20 }}
-            />
-        )
-    };
-    renderItem = ({item}) => {
-        return (
-            <ListItem item={item}/>
-        )
-    };
-
-    render() {
+    render(){
 
         const customStyle = {
 
-            topBar: [styles.topBar, {
-                height: getStatusBarHeight() + DIMENSION.TOPBAR.HEIGHT,
-                
+            themeTab: [styles.themeTab, {
+                backgroundColor: settings_this.state.color_theme.BUTTON,
+                shadowColor: settings_this.state.color_theme.APP_FOCUS
             }],
 
-            title: [styles.title, {
-                fontSize: DIMENSION.TITLE.SIZE,
-                paddingTop: getStatusBarHeight() + (DIMENSION.TOPBAR.HEIGHT - DIMENSION.TITLE.SIZE) / 2 - 3,
-                
+            buttonClose: [styles.buttonClose, {
+                color: settings_this.state.color_theme.APP_FOCUS
             }],
 
-            historyContainer: [styles.historyContainer, {
-                marginTop: getStatusBarHeight() + DIMENSION.TOPBAR.HEIGHT
+            titleText: [styles.titleText, {
+                color: settings_this.state.color_theme.APP_FOCUS
+            }],
+
+            divider: [styles.divider, {
+                borderBottomColor: settings_this.state.color_theme.BUTTON
             }]
 
         };
 
+        return(
 
-        return (
-            <View style={styles.container}>
-                //<StatusBar barStyle={statusTheme}/>
-                <View style={customStyle.topBar}/>
-                <Text style={customStyle.title}>Messages</Text>
-              
+          <View style={ styles.container}>
+                <ScrollView
+                    style={{
+                        padding: 20,
+                        backgroundColor: (settings_this.state.color_theme)? settings_this.state.color_theme.APP_BACKGROUND: null
+                    }}>
 
-            </View>
+
+                    <View style={styles.titleBar}>
+                        <Ionicons
+                            name="ios-close"
+                            style={customStyle.buttonClose}
+                            onPress={() => {
+                                this.props.navigation.goBack(null)
+                            }}/>
+
+                        <CenterText style={customStyle.titleText}> Settings </CenterText>
+                    </View>
+
+                    
+                    <CenterText style={customStyle.titleText}> Coming Up In The Future!</CenterText>
+
+                    <View style={styles.themeBox}>
+
+
+                    </View>
+
+
+                   
+                </ScrollView>
+          </View>
         );
     }
+
 }
 
-//var width=Dimensions.get("window").width;
+
+const wipeLogout ={
+    type: 'Navigation/NAVIGATE',
+    routeName: 'RootStack',
+    actions: {
+        type: 'Navigation/NAVIGATE',
+    }
+};
+
+
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        flexDirection: 'column'
-    },
-    topBar: {
         backgroundColor: null,
-        alignSelf: 'stretch',
-        height: null
-    },
-    rideHistStyle: {
         flex: 1,
-        aspectRatio: 0.5,
-        resizeMode: 'contain'
+
     },
-    title: {
-        color: null,
-        alignSelf: 'center',
+    buttonText: {
+        textAlign: 'center',
+        color: "#FFFFFF",
+        fontWeight: "700",
+        textShadowColor:'rgba(0, 0, 0, 0.7)',
+        textShadowOffset: {width: -1, height: 1},
+        textShadowRadius: 10,
+    },
+    themeBox: {
+        flexDirection: 'row',
+        marginLeft: Dimensions.get('window').width / 16,
+        marginRight: Dimensions.get('window').width / 16,
+        marginTop: 10,
+        marginBottom: 10,
+        //outline: 1,
+        zIndex: 5
+    },
+    themeTab: {
+        width: Dimensions.get('window').width / 4,
+        height: 50,
+        backgroundColor: null,
+        borderRadius: 5,
+        shadowColor: null,
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+        shadowRadius: 10,
+        shadowOpacity: 0.25,
+        flex: 1,
         justifyContent: 'center',
-        position: 'absolute',
-        fontSize: null,
-        paddingTop: null
+        marginLeft: 5,
+        marginRight: 5
     },
-    historyContainer: {
-        marginTop: null,
-        flex: 1
+    buttonClose: {
+        color: null,
+        fontSize: 50,
+        width: 100
+    },
+    titleBar: {
+        flexDirection: 'row'
+    },
+    titleText: {
+        fontSize: 20,
+        color: null
+    },
+    divider: {
+        borderBottomColor: null,
+        borderBottomWidth: 1,
+        marginLeft: Dimensions.get('window').width / 16,
+        marginRight: Dimensions.get('window').width / 16,
+        marginTop: 20,
+        marginBottom: 20
     }
 });
