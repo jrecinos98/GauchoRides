01b3e930f9fededa6730a52b2ae7c3a4c10829e2
Jose <jrecinos@umail.ucsb.edu>
2018-06-08 17:24:11 -0700

    Added a Utility test

--- tests/Utility/Utility_Test.js
+++ tests/Utility/Utility_Test.js
@@ -8,9 +8,9 @@ import Utility from "../../src/Utility"
 
 // Test extractCity function
 test('Test: extractCity(text)', () => {
-    const IV= [
+    const IV = [
         {
-         value: "1500 Sabado Tarde Road"
+            value: "1500 Sabado Tarde Road"
         },
         {
             value: "Isla Vista"
@@ -33,7 +33,7 @@ test('Test: extractCity(text)', () => {
             value: "USA"
         }
     ];
-    const LA= [
+    const LA = [
         {
             value: "Los Angeles"
         },
@@ -53,10 +53,17 @@ test('Test: extractCity(text)', () => {
         }
 
     ];
-    console.log(IV[0].value);
     expect(Utility.extractCity(IV)).toBe('Isla Vista, CA');
     expect(Utility.extractCity(AUSTIN)).toBe('Austin, TX');
     expect(Utility.extractCity(LA)).toBe('Los Angeles, CA');
     expect(Utility.extractCity(CA)).toBe('');
     expect(Utility.extractCity([{value: "CA"}])).toBe('');
 });
+
+test('Test: formatDate(date)', () => {
+    const CHRISTMAS= new Date(1545696000000);
+    const NEW_YEARS= new Date(1546214400000)
+    expect(Utility.formatDate(CHRISTMAS)).toBe('Dec 24, 04:00 PM');
+    expect(Utility.formatDate(NEW_YEARS)).toBe('Dec 30, 04:00 PM');
+});
+
